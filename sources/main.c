6c8b2d4a77564ad9bbde5d89a035ea7f2cf56016
JustHiro55 <hagi.hiro9829@gmail.com>
2024-03-02 18:19:53 +0900

    feat: Add minilibx basic support

--- sources/main.c
+++ sources/main.c
@@ -12,9 +12,46 @@
 
 #include "cub3d.h"
 
-int	main(void)
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
-	printf("Hello World!\n");
+	char	*dst;
 
-	return (EXIT_SUCCESS);
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+int	close_window(void *param)
+{
+	(void)param;
+	exit(EXIT_SUCCESS);
+}
+
+int	key_press(int keycode)
+{
+	if (keycode == KEY_ESC)
+	{
+		ft_printf("ESC key is pressed\n");
+		exit(EXIT_SUCCESS);
+	}
+	return (0);
+}
+
+int main(int argc, char **argv)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+								&img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_key_hook(mlx_win, key_press, NULL);
+	mlx_hook(mlx_win, 17, 0, close_window, NULL);
+	mlx_loop(mlx);
+
+	exit(EXIT_SUCCESS);
 }
