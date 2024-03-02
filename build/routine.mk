1a272d35dd89c67df9be6993c1300dd7f76c1453
JustHiro55 <hagi.hiro9829@gmail.com>
2024-03-02 17:33:58 +0900

    refactor: Separate Makefile

---
+++ build/routine.mk
@@ -0,0 +1,40 @@
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	@ $(MAKE) -C $(MLX_DIR)
+	@ $(CC) $(CFLAGS) -o $@ $^ -L$(MLX_DIR) -lmlx -framework OpenGL -framework AppKit
+	@ printf "$(CHECK) $(BLUE)Compiled $(NAME)!%-50.50s\n$(RESET)"
+
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
+	@ mkdir -p $(dir $@)
+	@ $(CC) $(CFLAGS) $(INC) -o $@ -c $<
+	$(call progress)
+
+$(OBJ_DIR)/%.o: $(GNL_DIR)/%.c
+	@ $(CC) $(CFLAGS) $(INC) -c $< -o $@
+	@ echo "$(GENERATE) $(YELLOW)Compiling GNL: $< $(RESET)"
+
+$(OBJ_DIR)/%.o: $(FT_PRINTF_DIR)/%.c
+	@ $(CC) $(CFLAGS) $(INC) -c $< -o $@
+	@ echo "$(GENERATE) $(YELLOW)Compiling ft_printf: $< $(RESET)"
+
+clean:
+	@ $(MAKE) -C $(MLX_DIR) clean
+	@ $(RM) $(OBJ_DIR)
+	@ echo "$(REMOVE) $(BLUE)Removing $(NAME) object files $(RESET)"
+
+fclean:
+	@ $(MAKE) -C $(MLX_DIR) fclean
+	@ $(RM) $(OBJ_DIR) $(NAME)
+	@ echo "$(REMOVE) $(BLUE)Removing $(NAME) object files and $(NAME) $(RESET)"
+
+re: fclean all
+
+debug: re
+
+address: re
+
+norm:
+	norminette sources includes libft
+
+.PHONY: all clean fclean re
