47d4053461667ef57327f7b3ebd37b80beae56ec
JustHiro55 <hagi.hiro9829@gmail.com>
2024-03-03 18:24:06 +0900

    feat: Compile with libft Makefile

--- build/progress.mk
+++ build/progress.mk
@@ -4,7 +4,7 @@ CURRENT_FILE = 1
 define progress
     @printf "$(GENERATE) $(YELLOW)Generating $(NAME) object files: %3d%% (%d/%d)$(RESET)\r" $$(($(CURRENT_FILE)*100/$(TOTAL_FILES))) $(CURRENT_FILE) $(TOTAL_FILES)
     @$(eval CURRENT_FILE=$(shell echo $$(($(CURRENT_FILE)+1))))
-    @if [ $(CURRENT_FILE) -gt $(TOTAL_FILES) ]; then \
-        printf "$(GENERATE) $(YELLOW)Generated $(NAME) object files!%-50.50s\n$(RESET)"; \
-    fi
+    # @if [ $(CURRENT_FILE) -gt $(TOTAL_FILES) ]; then \
+    #     printf "$(GENERATE) $(YELLOW)Generated $(NAME) object files!%-50.50s\n$(RESET)"; \
+    # fi
 endef
