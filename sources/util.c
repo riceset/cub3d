3908227ea1b5d50f308012a33ec287a2fe4e8819
JustHiro55 <JustHiro271@gmail.com>
2024-03-06 17:12:02 +0900

    fix: Corrected header issues

--- sources/util.c
+++ sources/util.c
@@ -3,44 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   util.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hiro <hiro@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: hhagiwar <hhagiwar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/05 15:07:17 by hiro              #+#    #+#             */
-/*   Updated: 2024/03/06 14:01:28 by hiro             ###   ########.fr       */
+/*   Created: 2024/03/06 17:10:05 by hhagiwar          #+#    #+#             */
+/*   Updated: 2024/03/06 17:10:08 by hhagiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void free_map(int **map, int height) {
-    int i;
+void	free_map(int **map, int height)
+{
+	int	i;
 
-    if (map) {
-        for (i = 0; i < height; i++) {
-            if (map[i]) {
-                free(map[i]);
-            }
-        }
-        free(map);
-    }
+	if (map)
+	{
+		for (i = 0; i < height; i++)
+		{
+			if (map[i])
+			{
+				free(map[i]);
+			}
+		}
+		free(map);
+	}
 }
 
-void ft_exit(char *err_msg, t_data *data) {
-    if (err_msg) {
-        perror(err_msg);
-    }
-
-    if (data) {
-        if (data->map) {
-            free_map(data->map, data->h_map);
-        }
-        if (data->player) {
-            free(data->player);
-        }
-        free(data);
-    }
-
-    exit(EXIT_FAILURE);
+void	ft_exit(char *err_msg, t_data *data)
+{
+	if (err_msg)
+	{
+		perror(err_msg);
+	}
+	if (data)
+	{
+		if (data->map)
+		{
+			free_map(data->map, data->h_map);
+		}
+		if (data->player)
+		{
+			free(data->player);
+		}
+		free(data);
+	}
+	exit(EXIT_FAILURE);
 }
 int	extension_check(char *filename)
 {
@@ -55,38 +62,43 @@ int	extension_check(char *filename)
 	return (ERROR);
 }
 
-void print_file(char *filename)
+void	print_file(char *filename)
 {
-    char *line;
-    int fd;
-    int line_number = 1;
-    printf("\n--------- Start of %s ---------\n\n", filename);
-    fd = open(filename, O_RDONLY);
-    while ((line = get_next_line(fd)) != NULL)
-    {
-        printf("%d: %s", line_number, line);
-        free(line);
-        line_number++;
-    }
-    printf("\n\n--------- End of %s ---------\n\n", filename);
-    close(fd);
+	char	*line;
+	int		fd;
+	int		line_number;
+
+	line_number = 1;
+	printf("\n--------- Start of %s ---------\n\n", filename);
+	fd = open(filename, O_RDONLY);
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		printf("%d: %s", line_number, line);
+		free(line);
+		line_number++;
+	}
+	printf("\n\n--------- End of %s ---------\n\n", filename);
+	close(fd);
 }
 
-void print_map(int **map, int w_map)
+void	print_map(int **map, int w_map)
 {
-    int i = 0;
-	int j = 0;
-    printf("\n--------- Start of Map ---------\n\n");
-    while(map[i] != NULL)
-    {
-		while(j < w_map)
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	printf("\n--------- Start of Map ---------\n\n");
+	while (map[i] != NULL)
+	{
+		while (j < w_map)
 		{
-	        printf("%d", map[i][j]);
+			printf("%d", map[i][j]);
 			j++;
 		}
 		printf("\n");
 		j = 0;
-        i++;
-    }
-    printf("\n--------- End of Map ---------\n\n");
+		i++;
+	}
+	printf("\n--------- End of Map ---------\n\n");
 }
