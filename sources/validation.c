3f335a29449415a0bf37809781ecd05229b49399
JustHiro55 <hagi.hiro9829@gmail.com>
2024-03-18 15:20:35 +0900

    fix: Fix error handling for map surrounded by walls

--- sources/validation.c
+++ sources/validation.c
@@ -3,47 +3,42 @@
 void flood_fill(int **map, int x, int y, int h_map, int w_map, int *status);
 int **copy_map(int **src, int h_map, int w_map);
 
-int validate_map(t_data *data)
-{
-	int status = 0;
-	int **map_copy = copy_map(data->map, data->h_map, data->w_map);
-	if (!map_copy)
-	{
-		ft_exit("Failed to copy map for validation", data);
-	}
-	flood_fill(map_copy, data->player->plyr_x, data->player->plyr_y, data->h_map, data->w_map, &status);
-	int i = 0;
-	while (i < data->h_map)
-	{
-		free(map_copy[i]);
-		i++;
-	}
-	free(map_copy);
-	return status;
+int validate_map(t_data *data) {
+    int status = 0;
+    int player_x = data->player->plyr_x / TILE_SIZE;
+    int player_y = data->player->plyr_y / TILE_SIZE;
+
+    int **map_copy = copy_map(data->map, data->h_map, data->w_map);
+    if (!map_copy) {
+        ft_exit("Failed to copy map for validation", data);
+    }
+
+    flood_fill(map_copy, player_x, player_y, data->h_map, data->w_map, &status);
+
+    int i = 0;
+    while (i < data->h_map) {
+        free(map_copy[i]);
+        i++;
+    }
+    free(map_copy);
+
+    return status;
 }
 
-void flood_fill(int **map, int x, int y, int h_map, int w_map, int *status)
-{
-	x /= TILE_SIZE;
-	y /= TILE_SIZE;
-	if (x < 0 || x >= w_map || y < 0 || y >= h_map || map[y][x] == FORBIDDEN_SPACE)
-	{
-		*status = 1;
-		return;
-	}
-	if (map[y][x] == -1 || map[y][x] == WALL)
-	{
-		return;
-	}
-
-	map[y][x] = -1;
-
-	flood_fill(map, x + 1, y, h_map, w_map, status);
-	flood_fill(map, x - 1, y, h_map, w_map, status);
-	flood_fill(map, x, y + 1, h_map, w_map, status);
-	flood_fill(map, x, y - 1, h_map, w_map, status);
+void flood_fill(int **map, int x, int y, int h_map, int w_map, int *status) {
+    if (x < 0 || x >= w_map || y < 0 || y >= h_map || map[y][x] == FORBIDDEN_SPACE) {
+        *status = 1;
+        return;
+    }
+    if (map[y][x] == -1 || map[y][x] == WALL) {
+        return;
+    }
+    map[y][x] = -1;
+    flood_fill(map, x + 1, y, h_map, w_map, status);
+    flood_fill(map, x - 1, y, h_map, w_map, status);
+    flood_fill(map, x, y + 1, h_map, w_map, status);
+    flood_fill(map, x, y - 1, h_map, w_map, status);
 }
-
 
 int **copy_map(int **src, int h_map, int w_map)
 {
