48820478a4562c5afe511fe3a05d3f0ca49c0ce8
JustHiro55 <hagi.hiro9829@gmail.com>
2024-05-30 17:58:47 +0900

    feat: Render corresponding textures based on wall direction

--- sources/graphics/draw/draw_textured_wall.c
+++ sources/graphics/draw/draw_textured_wall.c
@@ -1,70 +1,72 @@
 #include "cub3d.h"
 
-static int calculate_texture_coordinates(t_data *data, int ray, double *hitX, double *hitY, double *wallX, int *texX, t_texture *texture);
-static void draw_textured_pixels(t_mlx *img, int ray, int top_pixel, int bottom_pixel, int wall_top_pixel, int wall_bottom_pixel, t_texture *texture, int wallHeight, int texX);
+static void draw_textured_pixels(t_mlx *img, int ray, int direction, t_wall_data *wall_data, t_texture_data *tex_data);
+static int get_direction(t_data *data, int ray, t_texture_data *tex_data);
 
-void draw_textured_wall(t_mlx *img, int ray, int top_pixel, int bottom_pixel, int wall_top_pixel, int wall_bottom_pixel, t_texture *texture, t_data *data)
+void draw_textured_wall(t_mlx *img, int ray, t_wall_data *wall_data, t_texture_data *tex_data, t_data *data)
 {
-    int wallHeight;
-    int texX;
-    double hitX;
-    double hitY;
-    double wallX;
-    int direction;
-
-    wallHeight = wall_bottom_pixel - wall_top_pixel;
-    direction = calculate_texture_coordinates(data, ray, &hitX, &hitY, &wallX, &texX, texture);
-    draw_textured_pixels(img, ray, direction, top_pixel, bottom_pixel, wall_top_pixel, texture, wallHeight, texX);
+    int direction = get_direction(data, ray, tex_data);
+    draw_textured_pixels(img, ray, direction, wall_data, tex_data);
 }
 
-static void calculate_hits_and_texture(t_data *data, double ray_angle, double distance, int direction, double *hitX, double *hitY, double *wallX, int *texX, t_texture *texture)
+static void calculate_hits_and_texture(t_data *data, double ray_angle, double distance, int direction, t_texture_data *tex_data)
 {
     if (direction == EAST)
-    { // 東側の壁
-        *hitY = data->player->plyr_y - distance * sin(ray_angle);
-        *wallX = fmod(*hitY, TILE_SIZE);
-        *texX = -(int)((TILE_SIZE / 2 + *wallX) * texture->width / TILE_SIZE) % texture->width;
+    {
+        tex_data->hitY = data->player->plyr_y - distance * sin(ray_angle);
+        tex_data->wallX = fmod(tex_data->hitY, TILE_SIZE);
+        tex_data->texX = -(int)((TILE_SIZE / 2 + tex_data->wallX) * tex_data->texture[EAST]->width / TILE_SIZE) % tex_data->texture[EAST]->width;
     }
     else if (direction == WEST)
-    { // 西側の壁
-        *hitY = data->player->plyr_y - distance * sin(ray_angle);
-        *wallX = fmod(*hitY, TILE_SIZE);
-        *texX = (int)((TILE_SIZE / 2 + *wallX) * texture->width / TILE_SIZE) % texture->width;
+    {
+        tex_data->hitY = data->player->plyr_y - distance * sin(ray_angle);
+        tex_data->wallX = fmod(tex_data->hitY, TILE_SIZE);
+        tex_data->texX = (int)((TILE_SIZE / 2 + tex_data->wallX) * tex_data->texture[WEST]->width / TILE_SIZE) % tex_data->texture[WEST]->width;
     }
     else if (direction == SOUTH)
-    { // 南側の壁
-        *hitX = data->player->plyr_x + distance * cos(ray_angle);
-        *wallX = fmod(*hitX, TILE_SIZE);
-        *texX = (int)((TILE_SIZE / 2 + *wallX) * texture->width / TILE_SIZE) % texture->width;
+    {
+        tex_data->hitX = data->player->plyr_x + distance * cos(ray_angle);
+        tex_data->wallX = fmod(tex_data->hitX, TILE_SIZE);
+        tex_data->texX = (int)((TILE_SIZE / 2 + tex_data->wallX) * tex_data->texture[SOUTH]->width / TILE_SIZE) % tex_data->texture[SOUTH]->width;
     }
     else if (direction == NORTH)
-    { // 北側の壁
-        *hitX = data->player->plyr_x + distance * cos(ray_angle);
-        *wallX = fmod(*hitX, TILE_SIZE);
-        *texX = -(int)((TILE_SIZE / 2 + *wallX) * texture->width / TILE_SIZE) % texture->width;
+    {
+        tex_data->hitX = data->player->plyr_x + distance * cos(ray_angle);
+        tex_data->wallX = fmod(tex_data->hitX, TILE_SIZE);
+        tex_data->texX = -(int)((TILE_SIZE / 2 + tex_data->wallX) * tex_data->texture[NORTH]->width / TILE_SIZE) % tex_data->texture[NORTH]->width;
     }
 }
 
-static int calculate_texture_coordinates(t_data *data, int ray, double *hitX, double *hitY, double *wallX, int *texX, t_texture *texture)
+static int get_direction(t_data *data, int ray, t_texture_data *tex_data)
 {
     double ray_angle;
     int direction;
 
     ray_angle = data->player->angle + (data->player->fov_rd / 2) - (ray * data->player->fov_rd / WIDTH);
     direction = determine_wall_direction(data, ray_angle);
-    calculate_hits_and_texture(data, ray_angle, cast_ray(data, ray_angle), direction, hitX, hitY, wallX, texX, texture);
+    calculate_hits_and_texture(data, ray_angle, cast_ray(data, ray_angle), direction, tex_data);
     return direction;
 }
 
-static void draw_textured_pixels(t_mlx *img, int ray, int direction, int top_pixel, int bottom_pixel, int wall_top_pixel, t_texture *texture, int wallHeight, int texX)
+static void draw_textured_pixels(t_mlx *img, int ray, int direction, t_wall_data *wall_data, t_texture_data *tex_data)
 {
-    (void)direction; // texture
-    for (int pixel_y = top_pixel; pixel_y < bottom_pixel; pixel_y++)
+    int pixel_y;
+    int texY;
+    int color;
+    int draw_start = wall_data->top_pixel;
+    int draw_end = wall_data->bottom_pixel;
+
+    for (pixel_y = draw_start; pixel_y < draw_end; pixel_y++)
     {
-        int texY = ((pixel_y - wall_top_pixel) * texture->height) / wallHeight;
-        if (texY < 0) texY = 0;
-        if (texY >= texture->height) texY = texture->height - 1;
-        int color = get_texture_color(texture, texX, texY);
-        my_mlx_pixel_put(img, ray, pixel_y, color);
+        if (pixel_y >= 0 && pixel_y < HEIGHT) // ウィンドウの範囲内のピクセルのみ描画
+        {
+            texY = ((pixel_y - wall_data->top_pixel) * tex_data->texture[direction]->height) / wall_data->wall_height;
+            if (texY < 0)
+                texY = 0;
+            if (texY >= tex_data->texture[direction]->height)
+                texY = tex_data->texture[direction]->height - 1;
+            color = get_texture_color(tex_data->texture[direction], tex_data->texX, texY);
+            my_mlx_pixel_put(img, ray, pixel_y, color);
+        }
     }
 }
