c5118f404ee3dd21db99e5d35f530f736a031cd5
komeno <tkomeno@student.42tokyo.jp>
2024-07-02 12:39:10 +0900

    refactor: Fixed norm errors for load_texture_from_bmp except func encapsulation

--- sources/graphics/bmp/load_texture_from_bmp.c
+++ sources/graphics/bmp/load_texture_from_bmp.c
@@ -1,9 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   load_texture_from_bmp.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: riceset <tkomeno@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/02 11:20:51 by riceset           #+#    #+#             */
+/*   Updated: 2024/07/02 12:38:33 by riceset          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
+
+static int	bytes_to_int(byte *bytes, int num_bytes)
+{
+	int	i;
+	int	res;
+
+	i = 0;
+	res = 0;
+	while (i < num_bytes)
+	{
+		res |= bytes[i] << (8 * i);
+		i++;
+	}
+	return (res);
+}
 
 static void	read_bmp_header(int fd, int *width, int *height, int *has_alpha)
 {
 	unsigned char	header[54];
-	int				bitsPerPixel;
+	int				bits_per_pixel;
 	int				compression;
 
 	if (read(fd, header, 54) != 54)
@@ -12,11 +39,11 @@ static void	read_bmp_header(int fd, int *width, int *height, int *has_alpha)
 		close(fd);
 		exit(1);
 	}
-	*width = header[18] | (header[19] << 8) | (header[20] << 16) | (header[21] << 24);
-	*height = header[22] | (header[23] << 8) | (header[24] << 16) | (header[25] << 24);
-	bitsPerPixel = header[28] | (header[29] << 8);
-	compression = header[30] | (header[31] << 8) | (header[32] << 16) | (header[33] << 24);
-	if (bitsPerPixel == 32 && (compression == 3 || compression == 6))
+	*width = bytes_to_int(&header[18], 4);
+	*height = bytes_to_int(&header[22], 4);
+	bits_per_pixel = bytes_to_int(&header[28], 2);
+	compression = bytes_to_int(&header[30], 4);
+	if (bits_per_pixel == 32 && (compression == 3 || compression == 6))
 	{
 		*has_alpha = 1;
 	}
@@ -35,7 +62,7 @@ t_texture	*load_texture_from_bmp(const char *file_path)
 	int			fd;
 	int			has_alpha;
 	t_texture	*texture;
-	int			dataSize;
+	int			data_size;
 
 	fd = open(file_path, O_RDONLY);
 	if (fd < 0)
@@ -56,8 +83,8 @@ t_texture	*load_texture_from_bmp(const char *file_path)
 		perror("Provide a 24bit bitmap image");
 		exit(1);
 	}
-	dataSize = texture->width * texture->height * 3;
-	texture->data = malloc(dataSize);
+	data_size = texture->width * texture->height * 3;
+	texture->data = malloc(data_size);
 	if (!texture->data)
 	{
 		perror("Failed to allocate texture data");
@@ -65,7 +92,7 @@ t_texture	*load_texture_from_bmp(const char *file_path)
 		close(fd);
 		exit(1);
 	}
-	if (read(fd, texture->data, dataSize) != dataSize)
+	if (read(fd, texture->data, data_size) != data_size)
 	{
 		perror("Error reading texture data");
 		free(texture->data);
